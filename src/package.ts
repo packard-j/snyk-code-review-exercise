b36f0f5da03182ff38719b0ecf49088d54e44259
James Packard <40209595+packard-j@users.noreply.github.com>
2025-03-24 22:37:10 -0400

    add ideas

--- src/package.ts
+++ src/package.ts
@@ -29,7 +29,7 @@ export const getPackage: RequestHandler = async function (req, res, next) {
       // easiest structure to work with as an HTTP API consumer. The body of
       // the response could become quite large, containing many duplicate packages;
       // an extreme example would be querying for the "everything" package.
-      // A flatter response structure might be preferrable, e.g. with each package
+      // idea: A flatter response structure might be preferrable, e.g. with each package
       // referring to another top-level object by name & version.
       .json({ name, version, dependencies: dependencyTree });
   } catch (error) {
@@ -42,7 +42,7 @@ async function getDependencies(name: string, range: string): Promise<Package> {
     `https://registry.npmjs.org/${name}`,
   ).json();
 
-  // review: this might result in cyclic depndency resolution when encountering
+  // review: this can result in cyclic depndency resolution when encountering
   // packages containing a dependency with '*' as the specified version.
   // The "everything" package is again a good example.
   const v = maxSatisfying(Object.keys(npmPackage.versions), range);
@@ -53,6 +53,15 @@ async function getDependencies(name: string, range: string): Promise<Package> {
     // review: Sequentially fetching dependencies will cause a
     // networking bottleneck. It may be better to fetch dependencies
     // concurrently (up to a max amount depending on NPM's rate limits).
+    // idea: Traverse dependencies in breadth-first order, collecting the unique
+    // name-version pairs into a queue while enforcing uniqueness. While keeping
+    // track of the already-fetched (visited) dependencies as a cache,
+    // from the queue, fetch the next level of dependencies concurrently
+    // (possibly in batches), referencing the cache when a dependency is already
+    // visited.
+    // idea: depending on the expected load for this web server, an additional caching
+    // layer might be appropraite to support multiple instances of the web server
+    // sharing the same cache.
     for (const [name, range] of Object.entries(newDeps ?? {})) {
       // review: recusively traversing dependencies without maintaining
       // an accumulator for the already-fetched dependencies will result
